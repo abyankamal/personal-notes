dcb0bd03dea607a36a48e6f62b2fd66f971f6d27
abyankamal <abyankamal8@gmail.com>
2024-09-17 21:28:17 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+import NoteForm from "./components/NoteForm";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <NoteForm />
+    </div>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+export default function Header({ setSearchQuery }) {
+  return (
+    <header className="bg-white">
+      <nav
+        aria-label="Global"
+        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+      >
+        <div className="flex lg:flex-1">
+          <a href="#" className="-m-1.5 p-1.5">
+            <h1 className="text-xl font-bold">Personal Notes</h1>
+          </a>
+        </div>
+        <div className="flex mt-2">
+          <input
+            type="text"
+            onChange={(e) => setSearchQuery(e.target.value)}
+            placeholder="cari catatan disini"
+            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 placeholder:ml-2 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+        </div>
+      </nav>
+    </header>
+  );
+}
